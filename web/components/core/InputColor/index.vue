3e144665ee8d676ff3b76da985e455464f6c44d7
Rifqi22 <rifqinaufalfayyadh@gmail.com>
2025-11-20 11:50:37 +0700

    Web: fixing issue at input color

--- web/components/core/InputColor/index.vue
+++ web/components/core/InputColor/index.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import { useFloating, offset, flip } from "@floating-ui/vue";
 import IcArrowReg from "~/assets/icons/ic-arrow-reg.svg";
 
 defineProps<{
   modelValue: string;
   updateColor: (color: string) => void;
-  simple?: boolean;
 }>();
 const emit = defineEmits(["update:modelValue"]);
 
+const reference = ref(null);
+const floating = ref(null);
+const { floatingStyles } = useFloating(reference, floating, {
+  placement: "right-start",
+  middleware: [offset(0), flip()],
+});
 const isOpen = ref(false);
 </script>
 
@@ -21,25 +27,17 @@ const isOpen = ref(false);
     }"
   >
     <button
-      v-if="simple"
-      class="size-4 focus:outline-none border border-grey-600 bg-grey-700 rounded-sm"
-      :style="{
-        backgroundColor: modelValue,
-      }"
-    ></button>
-    <button
-      v-else
       class="w-full h-[26px] flex items-center gap-2 focus:outline-none px-2 py-1 border border-grey-600 bg-grey-700 rounded-sm"
     >
       <div
-        class="w-full h-3 rounded-sm border border-white/45"
+        class="w-full h-3 rounded-sm"
         :style="{
           backgroundColor: modelValue,
         }"
       ></div>
       <IcArrowReg
         :fontControlled="false"
-        class="w-4 h-4 rotate-180 text-grey-400"
+        class="w-4 h-4 rotate-180 text-grey-900"
       />
     </button>
 
@@ -50,7 +48,6 @@ const isOpen = ref(false);
           :updateColor="(color:string) => {
           updateColor(color)
           emit('update:modelValue', color);}"
-          @close="isOpen = false"
         />
       </div>
     </template>
